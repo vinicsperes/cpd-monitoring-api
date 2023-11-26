64d8f7701b1981cfd7e07f96d7f5ad998245d2c5
vinicsperes <peresvini2@gmail.com>
2023-11-26 20:18:06 -0300

    Change port.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
