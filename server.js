9b968743f6e55388a92185fe60d1ede432f68284
vinicsperes <peresvini2@gmail.com>
2023-11-26 16:11:46 -0300

    Fix message.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
